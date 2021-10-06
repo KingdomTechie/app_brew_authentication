0ff86d02000f4fb70d07d20c076de85a584f1e92
Keith Powell <keith@tapcart.co>
2021-10-05 23:04:12 -0700

    Created login logic - level 1 access

--- app.js
+++ app.js
@@ -25,6 +25,9 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+//------------------------//
+//        Routes          //
+//------------------------//
 app.get("/", (req, res) => {
 
     res.render("home")
@@ -35,12 +38,48 @@ app.get("/login", (req, res) => {
     res.render("login")
 });
 
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    User.findOne({email: username, password: password}, (err, foundUser) => {
+        if(err) {
+            console.log(err);
+            res.redirect("/login")
+        } else {
+            if(foundUser) {
+                if(foundUser.password === password) {
+                    res.render("secrets")
+                } else {
+                    res.render("login")
+                }
+            } else {
+                res.render("login")
+            }
+        }
+    });
+});
+
+app.get("/logout", (req, res) => {
+    res.render("login")
+});
+
 app.get("/register", (req, res) => {
 
     res.render("register")
 });
 
+app.post("/register", (req, res) => {
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.create({email: username, password: password}, (err, createdUser) => {
+        if(err) console.log(err);
+        res.render("secrets")
+    });
+});
+
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
-})
+});

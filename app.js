1b244d2876a3fe5aa94f7f193b30ddd0875ccc32
Keith Powell <keith@tapcart.co>
2021-10-05 22:04:21 -0700

    Added boilerplate code and instatiatied database

--- app.js
+++ app.js
@@ -2,11 +2,43 @@
 const express = require("express");
 const ejs = require("ejs");
 const app = express();
-app.use(urlencoded({extended: true}));
+const mongoose = require("mongoose")
+
+//------------------------//
+//   Database Config      //
+//------------------------//
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+})
+
+const User = new mongoose.model("User", userSchema)
+
+//------------------------//
+//  Express Middleware    //
+//------------------------//
+
+app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+app.get("/", (req, res) => {
 
+    res.render("home")
+});
+
+app.get("/login", (req, res) => {
+
+    res.render("login")
+});
+
+app.get("/register", (req, res) => {
+
+    res.render("register")
+});
 
 
 app.listen(3000, () => {
